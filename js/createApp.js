ec33578149c1e3101bbe0cdd35548b224bffc430
Ushi54 <regile.hidaka@gmail.com>
2021-12-15 22:15:05 +0900

    開発手順を確認する.pngの表示実装

--- js/createApp.js
+++ js/createApp.js
@@ -1,9 +1,11 @@
 //front-end create-app
 const idea = document.getElementById("idea");
+const procedure = document.getElementById("procedure");
 const hintCard1 = document.getElementById("hint-card1");
 const hintCard2 = document.getElementById("hint-card2");
 const hintCard3 = document.getElementById("hint-card3");
 const hintCard4 = document.getElementById("hint-card4");
+const hintCard5 = document.getElementById("hint-card5");
 idea.addEventListener("mouseover",function(event){
   hintCard1.classList.add("hint-card-display")
   hintCard2.classList.add("hint-card-display")
@@ -15,4 +17,11 @@ idea.addEventListener("mouseout",function(event){
   hintCard2.classList.remove("hint-card-display")
   hintCard3.classList.remove("hint-card-display")
   hintCard4.classList.remove("hint-card-display")
+});
+
+procedure.addEventListener("mouseover",function(event){
+  hintCard5.classList.add("hint-card-display")
+});
+procedure.addEventListener("mouseout",function(event){
+  hintCard5.classList.remove("hint-card-display")
 });
